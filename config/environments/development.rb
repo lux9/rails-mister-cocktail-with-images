77c4f21c7e21e35cd986a1ac0835b70914d05d98
lux <luischn94@gmail.com>
2021-01-21 16:19:13 -0300

    hosting test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.webpacker.check_yarn_integrity = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
